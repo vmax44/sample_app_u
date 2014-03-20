edb058981569a9a7bbf3c3a068bbd0deb65fde3c
alekseevmv <alekseevmv@ubuntu>
2014-03-20 22:14:13 +0400

    Finish sign-in-out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,8 @@ describe User do
   
   it {should respond_to(:authenticate)}
   
+  it {should respond_to(:remember_token)}
+  
   
   it {should be_valid}
   
@@ -104,6 +106,11 @@ describe User do
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
+  end
+  
+  describe "remember token" do
+    before {@user.save}
+    its(:remember_token) {should_not be_blank}
   end
 
 end
