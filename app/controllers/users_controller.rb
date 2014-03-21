86fb464847847e956a96f970bd8a3de9b5c4ae0e
Vmax44 <vmax44@tut.by>
2014-03-21 18:00:45 +0400

    begin users list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only:[:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
   def new
     @user=User.new
   end
   def show
     @user= User.find(params[:id])
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+    if @user.update_attributes(user_params)
+      flash[:success]="Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   def create
@@ -22,5 +39,17 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    
+    # Before filters
+
+    def signed_in_user
+      store_location
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
