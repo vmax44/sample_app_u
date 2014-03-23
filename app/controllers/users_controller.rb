6a380407cb0d8bacf94e3a2c1abb85bfca6e5378
alekseevmv <alekseevmv@ubuntu>
2014-03-24 00:11:31 +0400

    begin microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   end
   def show
     @user= User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def edit
@@ -54,11 +55,6 @@ class UsersController < ApplicationController
     
     # Before filters
 
-    def signed_in_user
-      store_location
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
-    
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   before_save {self.email=email.downcase}
   before_create :create_remember_token
   
@@ -10,6 +11,11 @@ class User < ActiveRecord::Base
   
   has_secure_password
   validates :password, length: {minimum: 6}
+  
+  def feed
+    # Это предварительное решение. См. полную реализацию в "Following users".
+    Micropost.where("user_id = ?", id)
+  end
   
   def User.new_remember_token
     SecureRandom.urlsafe_base64
